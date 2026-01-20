1db7872132e410e16451ab3ef5fd7b152fe66b33
d08008116-crypto <d08008116@gmail.com>
2026-01-20 20:36:22 +0100

    Update game.js

--- game.js
+++ game.js
@@ -1,10 +1,23 @@
-alert("GAME JS LOADED");
-
 const canvas = document.getElementById("game");
 const ctx = canvas.getContext("2d");
 
-canvas.width = 300;
-canvas.height = 300;
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
 
+// фон
+ctx.fillStyle = "black";
+ctx.fillRect(0, 0, canvas.width, canvas.height);
+
+// большой квадрат по центру
 ctx.fillStyle = "red";
-ctx.fillRect(50, 50, 200, 200);
+ctx.fillRect(
+  canvas.width / 2 - 100,
+  canvas.height / 2 - 100,
+  200,
+  200
+);
+
+// текст (важно!)
+ctx.fillStyle = "white";
+ctx.font = "30px Arial";
+ctx.fillText("JS WORKS", 50, 50);
